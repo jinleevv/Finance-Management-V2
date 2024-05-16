f8a4cfdaa4da08214c70d2d4e9dc808292dd2d2f
Jinwon Lee <jinwonlee0916@gmail.com>
2024-05-16 16:38:28 -0400

    update download transactions feature

--- finacne-management/src/features/Finance Department/DownloadTransactions.tsx
+++ finacne-management/src/features/Finance Department/DownloadTransactions.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { CalendarIcon } from "@radix-ui/react-icons";
 import { addDays, format } from "date-fns";
 import { DateRange } from "react-day-picker";
@@ -15,15 +14,29 @@ import * as XLSX from "xlsx";
 import { useHooks } from "@/hooks";
 import { toast } from "sonner";
 import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
 export function DownloadTransaction() {
   const { clientI } = useHooks();
-  const [date, setDate] = React.useState<DateRange | undefined>({
+  const [date, setDate] = useState<DateRange | undefined>({
     from: new Date(),
     to: addDays(new Date(), 20),
   });
+  const [totalChecked, setTotalChecked] = useState<boolean>(true);
+  const [employeeChecked, setEmployeeChecked] = useState<boolean>(false);
 
-  async function handleClick() {
+  function handleTotalCheckedClicked() {
+    setTotalChecked(true);
+    setEmployeeChecked(false);
+  }
+
+  function handleEmployeeCheckedClicked() {
+    setTotalChecked(false);
+    setEmployeeChecked(true);
+  }
+
+  async function handleTotalClick() {
     try {
       const data = JSON.stringify({
         date_from: date.from.toISOString().split("T")[0],
@@ -70,13 +83,86 @@ export function DownloadTransaction() {
     }
   }
 
+  async function handleEmployeeClick() {
+    try {
+      const data = JSON.stringify({
+        date_from: date.from.toISOString().split("T")[0],
+        date_to: date.to.toISOString().split("T")[0],
+      });
+
+      await clientI
+        .post("/api/download-transaction/", data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then((res) => {
+          if (res.data.data.length !== 0) {
+            const jsonData = res.data.data;
+
+            // Group data by 'Full Name'
+            const groupedData = jsonData.reduce(
+              (acc: { [key: string]: any[] }, item: any) => {
+                const { "Full Name": fullName, ...rest } = item; // Extract 'Full Name' and remove it from rest
+                if (!acc[fullName]) {
+                  acc[fullName] = [];
+                }
+                acc[fullName].push({ fullName, ...rest });
+                return acc;
+              },
+              {}
+            );
+
+            // Iterate over each group and create a separate Excel file
+            Object.keys(groupedData).forEach((fullName) => {
+              const cleanedData = groupedData[fullName];
+              const worksheet = XLSX.utils.json_to_sheet(cleanedData);
+              const workbook = XLSX.utils.book_new();
+              XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+              // Buffer to store the generated Excel file
+              const excelBuffer = XLSX.write(workbook, {
+                bookType: "xlsx",
+                type: "array",
+              });
+              const blob = new Blob([excelBuffer], {
+                type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8",
+              });
+
+              const nameDate = new Date();
+              const fileDownloadName = `${fullName}_Transactions_List_${
+                nameDate.toISOString().split("T")[0]
+              }.xlsx`;
+              saveAs(blob, fileDownloadName);
+            });
+          }
+        });
+    } catch (err) {
+      toast("Something went wront, please contact IT department");
+    }
+  }
+
   return (
     <section className="w-full">
       <div className="mt-12 ml-10 mr-10">
         <Label className="grid text-2xl font-bold">Download Transactions</Label>
         <Label className="ml-1">Download Total Transaction Information</Label>
       </div>
-      <div className="flex w-full mt-12 pl-10 pr-10 gap-3">
+      <div className="flex mt-12 pl-10 pr-10 gap-3">
+        <div className="flex gap-1.5">
+          <Checkbox
+            checked={totalChecked}
+            onClick={handleTotalCheckedClicked}
+          />{" "}
+          <Label className="mt-0.5">Total File</Label>
+        </div>
+        <div className="flex gap-1.5">
+          <Checkbox
+            checked={employeeChecked}
+            onClick={handleEmployeeCheckedClicked}
+          />{" "}
+          <Label className="mt-0.5">By Each Employee</Label>
+        </div>
+      </div>
+      <div className="flex w-full mt-2 pl-10 pr-10 gap-3">
         <div className="w-1/2">
           <Popover>
             <PopoverTrigger asChild>
@@ -116,9 +202,15 @@ export function DownloadTransaction() {
           </Popover>
         </div>
         <div className="flex w-1/2">
-          <Button onClick={handleClick} className="">
-            Download
-          </Button>
+          {totalChecked ? (
+            <Button onClick={handleTotalClick} className="">
+              Download
+            </Button>
+          ) : (
+            <Button onClick={handleEmployeeClick} className="">
+              Download
+            </Button>
+          )}
         </div>
       </div>
     </section>
